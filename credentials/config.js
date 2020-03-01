cbd3703abdcafed7c2a061ea9b035cb47a86858b
paul garcia <pbrain19@gmail.com>
2020-02-29 22:48:15 -0800

    adjust env variable

--- credentials/config.js
+++ credentials/config.js
@@ -4,7 +4,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   REDIS_URL:
     process.env.REDIS_URL ||
-    'redis://h:p0f51bb4f5f81826ad572e2f2c241ef369b064632d3fa0bfc29e3433c3743ec02@ec2-3-218-228-83.compute-1.amazonaws.com:30769',
+    'redis://h:p0f51bb4f5f81826ad572e2f2c241ef369b064632d3fa0bfc29e3433c3743ec02@ec2-3-214-235-220.compute-1.amazonaws.com:32749',
   DATABASE_URL:
     process.env.DATABASE_URL ||
     'postgres://ozgknkxymsmihz:132bac14926e83ed9db1a5d80fba4216a5cd73c316a83746f069d1cda1a993c4@ec2-107-21-235-87.compute-1.amazonaws.com:5432/d7bt0archdinll',
