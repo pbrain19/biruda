fcb8cd8f5d4bb3f04327680a6ef6c304b4d82980
paul garcia <pbrain19@gmail.com>
2020-03-01 13:26:43 -0800

    remove index

--- handlers/listings/create.ts
+++ handlers/listings/create.ts
@@ -3,9 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import models from '../../models';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const payload = req.body;
+  const payload = req.body;
 
-    const listingInstance = await models.Listings.create(payload);
+  const listingInstance = await models.Listings.create(payload);
 
-    res.status(200).json(listingInstance);
+  res.status(200).json(listingInstance);
 };
