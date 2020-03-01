5d9f08342b3a2c15b71045276c4b6d0ac928043d
Raya Anderson <rayazand@gmail.com>
2020-03-01 12:06:45 -0800

    fixing index model

--- models/index.ts
+++ models/index.ts
@@ -4,10 +4,16 @@ import commitment from './commitment';
 import user from './user';
 import listings from './listings';
 import bid from './bid';
+import businessProfile from './business-profile';
+import listingOptions from './listing-options';
+import pastProjects from './past-project';
 
 export default {
   Commitment: commitment(sequelize),
   User: user(sequelize),
   Listings: listings(sequelize),
   Bid: bid(sequelize),
+  BusinessProfile: businessProfile(sequelize),
+  ListingOptions: listingOptions(sequelize),
+  PastProjects: pastProjects(sequelize)
 };
